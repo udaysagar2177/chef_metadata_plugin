c20c7ae7a4b3622c9ffa10d9ac352d571dd55b71
udaysagar2177 <udaysagar.2177@gmail.com>
2015-11-24 16:26:10 -0800

    Customizable list of attributes

--- api.py
+++ api.py
@@ -12,41 +12,75 @@ class Metadata(object):
 		chef_node_roles: abcd
 		chef_node_tags: abcd
 	"""
+	config = []
+	organization = ''
+	nodes_metadata = []
 
-	def __init__(self, chefUniqueId, chef_environment, chef_node_roles,
-		chef_node_tags, ):
-		self.chefUniqueId     = chefUniqueId
-		self.chef_node_environment = chef_environment
-		self.chef_node_roles  = chef_node_roles
-		self.chef_node_tags   = chef_node_tags
+	def __init__(self):
+		self.api = autoconfigure()
 
 
-def main():
-	api = autoconfigure()
-	nodes = []
-	collectMetadata(api, nodes)
-	# get the token as argument
-	printNodes(nodes)
-	
-def printNodes(nodes):
-	for node in nodes:
-		print "chefUniqueId: " + node.chefUniqueId
-		print("chef_environment: " + node.chef_node_environment)
-		print("roles: "+str(node.chef_node_roles))
-		print("tags: "+str(node.chef_node_tags))
-		print("******")
+	def run(self):
+		self.readConfig()
+		self.collectMetadata()
+		# get the token as argument
+		#printNodes(nodes)
+		
+	def readConfig(self):
+		self.config = []
+		with open('metadata.config') as f:
+			lines = f.readlines()
+			for line in lines:
+				if not line.startswith("#"):
+					if line != '\n':
+						self.config.append(line.rstrip('\n'))
 
-def collectMetadata(api, nodes):
-	# Get the organizations
-	organization_details =  api.api_request('GET', '')
-	organization = organization_details['name']
-	pprint('Organization: ' + organization)
-	print "******"
-	chefUniqueId = ''
-	nodes_details = api.api_request('GET', '/nodes')
-	for node_name in nodes_details.keys():
-		chefUniqueId = organization+"_"+node_name
-		node_details = (api.api_request('GET','/nodes/'+node_name))
+	def printNodes(self, nodes):
+		for node in nodes:
+			print "chefUniqueId: " + node.chefUniqueId
+			print("chef_environment: " + node.chef_node_environment)
+			print("roles: "+str(node.chef_node_roles))
+			print("tags: "+str(node.chef_node_tags))
+			print("******")
+
+	def collectMetadata(self):
+		# Get the organizations
+		organization_details =  self.api.api_request('GET', '')
+		self.organization = organization_details['name']
+		nodes = self.api.api_request('GET', '/nodes')
+		for node_name in nodes.keys():
+			self.getNodeInformation(node_name)
+
+	def getNodeInformation(self, node_name):
+		chefUniqueId = self.organization+"_"+node_name
+		node_details = (self.api.api_request('GET','/nodes/'+node_name))
+		#pprint(node_details)
+		nodeInformation = {}
+		nodeInformation['chefUniqueId'] = chefUniqueId
+		for attribute in self.config:
+			nodeInformation[attribute] = self.getAttributeValue(
+				self.buildFullAttribute(attribute), node_details);
+		print nodeInformation
+
+	def buildFullAttribute(self, attribute):
+		normal = ['tags']
+		base = ['chef_environment', 'chef_type', 'default', 'json_class',
+		'name', 'override', 'run_list']
+		if attribute in normal:
+			return 'normal.'+attribute
+		elif attribute in base:
+			return attribute
+		else:
+			return 'automatic.'+attribute
+
+	def getAttributeValue(self, attribute, node_details):
+		tokens = attribute.split('.')
+		tempValue = node_details
+		for token in tokens:
+			tempValue = tempValue[token]
+		return tempValue
+
+	def abcd(self):
 		chef_node_environment = node_details['chef_environment']
 		chef_node_roles = node_details['run_list']
 		chef_node_tags = node_details['normal']['tags']
@@ -54,5 +88,5 @@ def collectMetadata(api, nodes):
 			chef_node_roles, chef_node_tags)
 		nodes.append(node)
 
-
-main()
+m = Metadata()
+m.run()
