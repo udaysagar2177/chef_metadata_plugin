faf8b83c71d686167d41ce77c878d823e5c163ea
udaysagar2177 <udaysagar.2177@gmail.com>
2015-12-01 11:41:48 -0800

    Code is ready
    Using Node.attributes from PyChef to get the attribute values
    Now running the program in a loop
    Call Signalfx API only if there is an update in the metadata
    Store the new metadata to a pickle

--- api.py
+++ api.py
@@ -1,21 +1,31 @@
 from chef import autoconfigure, Node
 from pprint import pprint
+from time import sleep
 import logging
 import sys
+import getopt
+import requests
+import copy
+import re
+import pickle
+
 
 class Metadata(object):
 	"""
-	The metadata information of a chef node
-
-	Attributes:
-		
+	Collect metadata of a chef node
 	"""
-	CONFIG_FILE = 'metadata.config'
-	LOG_FILE = 'metadata.log'
+	CONFIG_FILE = 'configuration.txt'
+	LOG_FILE = '/var/log/ChefMetadata.log'
+	URL = 'http://lab-api.corp.signalfuse.com:8080/v1/dimension'
+	PICKLE_FILE = 'pk_metadata.pk'
+	SLEEP_DURATION = 60 			# IN SECONDS
+	propertyNamePattern = re.compile('^[a-zA-Z_][a-zA-Z0-9_-]*$')
+	config = []
+	organization = ''
+	nodes_metadata = []
 
 	logger = logging.getLogger(__name__)
 	logger.setLevel(logging.INFO)
-
 	handler = logging.FileHandler(LOG_FILE)
 	handler.setLevel(logging.INFO)
 	formatter = logging.Formatter(
@@ -23,40 +33,109 @@ class Metadata(object):
 	handler.setFormatter(formatter)
 	logger.addHandler(handler)
 
-	config = []
-	organization = ''
-	nodes_metadata = []
-
-	def __init__(self):
+	def __init__(self, ACCESS_TOKEN):
 		self.api = autoconfigure()
+		self.ACCESS_TOKEN = ACCESS_TOKEN
 
 	def run(self):
+		self.nodes_metadata = []
 		self.readConfig()
-		self.collectMetadata()
-		# get the token as argument
-		#printNodes(nodes)
+		self.collectMetadataFromChef()
+		for nodeInformation in self.nodes_metadata:
+			self.sendMetadataToSignalfx(nodeInformation)
+		self.saveMetadata()
+
+	def saveMetadata(self):
+		pickleData = {}
+		for nodeInformation in self.nodes_metadata:
+			pickleData[nodeInformation['chefUniqueId']] = nodeInformation
+			pickleData[nodeInformation['chefUniqueId']].pop('chefUniqueId')
+		output = open(self.PICKLE_FILE, 'wb')
+		pickle.dump(pickleData, output)
+		self.logger.info('Saved updated metadata to '+self.PICKLE_FILE)
+		output.close()
+
+	def sendMetadataToSignalfx(self, nodeInformation):
+		headers = {
+			'X-SF-Token':self.ACCESS_TOKEN,
+		}
+		resp = self.getSignalfxObjectId(nodeInformation, headers)
+		if len(resp.json()['rs']) == 0:
+			self.logger.info('Signalfx does not have your dimension'
+				+'object chefUniqueId:'+nodeInformation['chefUniqueId'])
+			return
+		signalfxObjectId = resp.json()['rs'][0]
+		self.logger.info("ObjectID for "+nodeInformation['chefUniqueId']
+			+" is "+signalfxObjectId)
+		new_metadata = self.checkForUpdatesInMetadata(copy.deepcopy(nodeInformation))
+		if new_metadata:
+			resp = requests.patch(self.URL+'/'+signalfxObjectId, params=new_metadata
+				, headers=headers)
+		else:
+			self.logger.info('No new metadata is found for '+nodeInformation['chefUniqueId'])
+
+	def checkForUpdatesInMetadata(self, current_data):
+		inputPickle = open(self.PICKLE_FILE, 'rb')
+		self.logger.info('Reading previous metadata from '+self.PICKLE_FILE)
+		savedMetadata = pickle.load(inputPickle)
+		inputPickle.close()
+		if current_data['chefUniqueId'] not in savedMetadata:
+			return current_data
+		previous_data = savedMetadata[current_data['chefUniqueId']]
+		for key in previous_data.keys():
+			if key in current_data and current_data[key] == previous_data[key]:
+				current_data.pop(key)
+		current_data.pop('chefUniqueId')
+		return current_data
+
+	def getSignalfxObjectId(self, nodeInformation, headers):
+		params = {
+			'query':'chefUniqueId:'+nodeInformation['chefUniqueId'],
+			'getIDs':'true'
+		}
+		resp = requests.get(self.URL, params=params, headers=headers)
+		if resp.status_code != 200:
+			self.logger.error('Unable to get ID of'+
+				'chefUniqueId object from Signalfx')
+			print(resp.raise_for_status())
+			self.exitNow()
+		return resp
 		
 	def readConfig(self):
 		self.config = []
-		with open('metadata.config') as f:
+		with open(self.CONFIG_FILE, 'r') as f:
 			lines = f.readlines()
 			for line in lines:
-				if not line.startswith("#"):
-					if line != '\n':
-						self.config.append(line.rstrip('\n'))
+				if not line.startswith("#") and line != '\n':
+					attribute = line.rstrip('\n')
+					if self.checkPropertyNameSyntax(attribute
+						.replace('.', '_')):
+						self.config.append(attribute)
+
+	def checkPropertyNameSyntax(self, attribute):
+		if not self.propertyNamePattern.match(attribute):
+			self.logger.error('Invalid attribute name '
+				+ attribute
+				+'Attribute names should follow '
+				+'the regex pattern ^[a-zA-Z_][a-zA-Z0-9_-]*$')
+			return False
+		return True
+
+	def exitNow(self):
+		print("Error logged into the log file! Exiting...");
+		sys.exit(1)
 
 	def apiGetRequest(self, endpoint):
 		value = None
 		try:
 			value = self.api.api_request('GET', endpoint)
 		except Exception as e:
-			self.logger.error('Unable to perform api GET request', exc_info=True)
-			print("Error logged into the log file! Exiting...");
-			sys.exit(1)
+			self.logger.error('Unable to perform api GET request'
+				, exc_info=True)
+			self.exitNow()
 		return value
 
-	def collectMetadata(self):
-		# Get the organizations
+	def collectMetadataFromChef(self):
 		organization_details =  self.apiGetRequest('')
 		self.organization = organization_details['name']
 		nodes = self.apiGetRequest('/nodes')
@@ -65,25 +144,23 @@ class Metadata(object):
 
 	def getNodeInformation(self, node_name):
 		chefUniqueId = self.organization+"_"+node_name
-		node_details = self.apiGetRequest('/nodes/'+node_name)
+		node_details = Node(node_name)
 		nodeInformation = {}
 		nodeInformation['chefUniqueId'] = chefUniqueId
+		nodeInformation['chef_environment'] = node_details.chef_environment
 		for attribute in self.config:
-			nodeInformation[attribute] = self.getAttributeValue(
-				self.buildFullAttribute(attribute), node_details);
-		print nodeInformation
-		# add this dictionary to an array
+			attributeValue = self.getAttributeValue(
+				attribute, node_details)
+			if attributeValue:
+				attribute = self.adjustAttributeName(attribute)
+				nodeInformation[attribute] = attributeValue
+		self.nodes_metadata.append(nodeInformation)
 
-	def buildFullAttribute(self, attribute):
-		normal = ['tags']
-		base = ['chef_environment', 'chef_type', 'default', 'json_class',
-		'name', 'override', 'run_list']
-		if attribute in normal:
-			return 'normal.'+attribute
-		elif attribute in base:
-			return attribute
-		else:
-			return 'automatic.'+attribute
+	def adjustAttributeName(self, attribute):
+		attribute = attribute.replace('.', '_')
+		if not attribute.startswith('chef_'):
+			attribute = 'chef_' + attribute
+		return attribute
 
 	def getAttributeValue(self, attribute, node_details):
 		tokens = attribute.split('.')
@@ -92,10 +169,40 @@ class Metadata(object):
 			try:
 				tempValue = tempValue[token]
 			except Exception as e:
-				self.logger.error('Invalid attribute is listed in '+self.CONFIG_FILE, exc_info=True)
-				print("Error logged into the log file! Exiting...");
-				sys.exit(1)
-		return tempValue
+				self.logger.error('Invalid attribute is listed in '
+					+self.CONFIG_FILE, exc_info=True)
+				return None
+		if isinstance(tempValue, dict):
+			self.logger.error('Attribute value for '+attribute+' cannot be a dictionary!')
+			return None
+		if isinstance(tempValue, list) and not (any(isinstance(x, dict) for x in tempValue)):
+			return '$'.join(tempValue)
+		return str(tempValue)
 
-m = Metadata()
-m.run()
+
+def main(argv):
+	FILE_NAME = sys.argv[0]
+	try:
+		opts, argv = getopt.getopt(argv, "ht:", ["accessToken="])
+	except getopt.GetoptError:
+		print 'Usage: python '+FILE_NAME+' -t <ACCESS_TOKEN>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'Usage: python '+FILE_NAME+' -t <ACCESS_TOKEN>'
+			sys.exit()
+		elif opt in ["-t", "accessToken"]:
+			ACCESS_TOKEN = arg
+	if ACCESS_TOKEN:
+		m = Metadata(ACCESS_TOKEN)
+		while True:
+			m.run()
+			sleep(m.SLEEP_DURATION)
+	else:
+		print 'Enter valid Access Token!'
+		print 'Usage: python '+FILE_NAME+' -t <ACCESS_TOKEN>'
+		sys.exit(2)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
